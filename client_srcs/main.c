205b5dc3acae5c22c38c1cba23004d3770bc93ae
public-jun <generalpublicjunjun@gmail.com>
2021-06-07 23:46:39 +0900

    print a

--- client_srcs/main.c
+++ client_srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 17:16:14 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/06/04 20:37:27 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/06/07 23:20:39 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,40 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <signal.h>
+#include <stdio.h>
 #include "../libft/libft.h"
+
+// 'a'を送信してみる
+void	send_a(pid_t server_pid)
+{
+	unsigned char byte = 'a'; //0b01100001
+
+	uint8_t	counter;
+
+	counter = 1 << 7;
+	while (counter)
+	{
+		if (byte & counter)
+		{
+			if (kill(server_pid, SIGUSR1) < 0)
+			{
+				perror("kill");
+				ft_putstr_fd("kill error\n", 2);
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			if (kill(server_pid, SIGUSR2) < 0)
+			{
+				ft_putstr_fd("kill error\n", 2);
+				exit(EXIT_FAILURE);
+			}
+		}
+		counter >>= 1;
+		usleep(600);
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -44,11 +77,12 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("Invalid server PID\n", 2);
 		exit(EXIT_FAILURE);
 	}
+	send_a(pid);
 	//killでserverに命令してみる
-	if (kill(pid, SIGUSR1) < 0)
-	{
-		ft_putstr_fd("kill error\n", 2);
-		exit(EXIT_FAILURE);
-	}
+	// if (kill(pid, SIGUSR1) < 0)
+	// {
+	// 	ft_putstr_fd("kill error\n", 2);
+	// 	exit(EXIT_FAILURE);
+	// }
 	return (0);
 }
